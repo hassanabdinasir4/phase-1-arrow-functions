2ec1216484ee9ecd6284e1fc71dac746efb17afe
hassanabdinasir4 <hassan.hassan@student.moringaschool.com>
2022-11-27 10:31:38 +0300

    Done

---
+++ index.js
@@ -0,0 +1,18 @@
+function divide(){
+    const divide = 2000 / 100
+    return 20
+}
+function square (){
+    let x = 2
+const square = (x)
+return x * x    
+   
+}
+
+
+function add(){
+    let a = 3
+    let b = 4
+ const add = (a,b)
+ return a + b
+}
